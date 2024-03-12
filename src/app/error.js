cc23aaeda4fa669a5e89a7f1922f216c712d8c80
nayon117 <hasibul.nayon1@gmail.com>
2024-03-12 09:02:08 +0600

    create custom error page

---
+++ src/app/error.js
@@ -0,0 +1,9 @@
+"use client";
+const ErrorPage = () => {
+    return(
+        <div>
+             <h1 className="text-red-500 text-4xl">something went wrong</h1>
+        </div>
+    )
+}
+export default ErrorPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Counter from "@/components/Counter/Counter";
 
 const HomePage = () => {
+  // throw new Error( );
   return (
     <div>
       <h1 className="text-2xl"> This is Next app </h1>
