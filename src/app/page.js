92c524fd1953423c41763b3d8967e92b8b20e12a
nayon117 <hasibul.nayon1@gmail.com>
2024-03-12 08:33:37 +0600

    create a counter app and learn to convert server to client comp

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+"use client";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [counter,setCounter] = useState(0);
   return(
     <div>
        <p> This is Next app </p>
+       <h3>Counter:{counter}</h3>
+       <button className="btn bg-blue-600 px-2 py-1" onClick={()=>setCounter(counter+1)}>Increase</button>
+       <button className="btn bg-blue-600 px-2 py-1" onClick={()=>setCounter(counter-1)}>Decrease</button>
     </div>
   )
 }
